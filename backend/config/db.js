155b975d8f59cc2ce639982d669622609811f6a4
Aman Chauhan <actehri0@gmail.com>
2026-08-28 22:30:45 +0530

    db connection reconfig

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,57 @@
+// import mongoose from "mongoose";
+
+// async function connectDB() {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+//         console.log("successfully connected to database");
+//     } catch (error) {
+//         console.log("Error connecting to database");
+//         console.error(error.message);
+//         process.exit(1);
+//     }
+
+// }
+
+// export default connectDB;
+
 import mongoose from "mongoose";
 
-async function connectDB() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("successfully connected to database");
-    } catch (error) {
-        console.log("Error connecting to database");
-        console.error(error.message);
-        process.exit(1);
-    }
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false, 
+    };
+
+    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongooseInstance) => {
+      console.log("successfully connected to database");
+      return mongooseInstance;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null; 
+    console.log("Error connecting to database");
+    console.error(error.message);
+ 
+    throw error;
+  }
+
+  return cached.conn;
 }
 
 export default connectDB;
